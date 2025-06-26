1a9cbbf7b5a092d8df73e64004ec610cc76f1585
chuongle <chuongle@Chuongs-Mac-mini.local>
2025-06-26 08:56:30 -0500

    feat: mock data for coffee pos system

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import categories from './categories.json';
+import items from './items.json';
+import variants from './variants.json';
+
+export { categories, items, variants };
+export type { Category, Item, Size, Variant } from './types';
